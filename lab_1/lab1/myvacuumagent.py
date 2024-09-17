0245f431bff98684cd0c6e861e2fdc790c25b667
erik <you@example.com>
2024-09-17 16:57:43 +0200

    return home function almost works

--- lab_1/lab1/myvacuumagent.py
+++ lab_1/lab1/myvacuumagent.py
@@ -16,9 +16,7 @@ AGENT_DIRECTION_EAST = 1
 AGENT_DIRECTION_SOUTH = 2
 AGENT_DIRECTION_WEST = 3
 
-AGENT_ACTION_LEFT = 0
-AGENT_ACTION_FORWARD = 1
-AGENT_ACTION_RIGHT = 2
+MAX_ITERATIONS = 1000
 
 def direction_to_string(cdr):
     cdr %= 4
@@ -50,7 +48,6 @@ class MyAgentState:
         # Metadata
         self.world_width = width
         self.world_height = height
-
 
     """
     Update perceived agent location
@@ -115,18 +112,25 @@ class MyVacuumAgent(Agent):
     def __init__(self, world_width, world_height, log):
         super().__init__(self.execute)
         self.initial_random_actions = 10
-        self.iteration_counter = 1000
+        self.iteration_counter = MAX_ITERATIONS
         self.state = MyAgentState(world_width, world_height)
         self.log = log
 
-    # ***INITIALIZE HEATMAP AND frame_counter HERE***
+        # Initialize heatmap
         plt.ion()  # Turn on interactive mode 
         self.fig, self.ax = plt.subplots(figsize=(8, 6))
         self.heatmap = None 
         self.fig.canvas.draw()  
 
-        self.heatmap_update_frequency = 5
+        self.heatmap_update_frequency = 1
         self.frame_counter = 0
+
+        self.heatmap_reset = False
+
+        self.turns_without_forward = 0
+        self.max_turns_without_forward = 5
+
+        self.following_wall = False
 
     def move_to_random_start_position(self, bump):
         action = random.uniform(0, 1)
@@ -158,7 +162,7 @@ class MyVacuumAgent(Agent):
 
         # Move agent to a randomly chosen initial position
         if self.initial_random_actions > 0:
-            self.log("Moving to random start position ({} steps left)".format(self.initial_random_actions))
+            self.log(f"Moving to random start position, ({self.initial_random_actions} steps left).")
             return self.move_to_random_start_position(bump)
 
         # Finalize randomization by properly updating position (without subsequently changing it)
@@ -181,18 +185,16 @@ class MyVacuumAgent(Agent):
             if self.iteration_counter == 0:
                 self.iteration_counter -= 1
                 self.log("Iteration counter is now 0. Halting!")
-                self.log("Performance: {}".format(self.performance))
+                self.log(f"Performance: {self.performance}")
             self.state.print_visit_count()
-            self.log("Performance: {}".format(self.performance))
+            self.log(f"Performance: {self.performance}")
             return ACTION_NOP
-
 
         self.iteration_counter -= 1
         
         self.update_heat()
 
-        self.frame_counter += 1
-        if self.frame_counter % self.heatmap_update_frequency == 0:
+        if self.iteration_counter % self.heatmap_update_frequency == 0:
             self.update_heatmap()
         
         # Track position of agent
@@ -213,36 +215,46 @@ class MyVacuumAgent(Agent):
         left_offset = [(-1, 0), (0, -1), (1, 0), (0, 1)][self.state.direction]
         front_offset = [(0, -1), (1, 0), (0, 1), (-1, 0)][self.state.direction]
         right_offset = [(1, 0), (0, 1), (-1, 0), (0, -1)][self.state.direction]
+        """
         left_tile = self.state.world[self.state.pos_x + left_offset[0]][self.state.pos_y + left_offset[1]]
         front_tile = self.state.world[self.state.pos_x + front_offset[0]][self.state.pos_y + front_offset[1]]
         right_tile = self.state.world[self.state.pos_x + right_offset[0]][self.state.pos_y + right_offset[1]]
-        tiles = [left_tile, front_tile, right_tile]
+        """
         offsets = [left_offset, front_offset, right_offset]
 
         if bump:
             # Mark the tile at the offset from the agent as a wall (since the agent bumped into it)
             self.state.update_world(self.state.pos_x + front_offset[0], self.state.pos_y + front_offset[1], AGENT_STATE_WALL)
 
-        self.log("Position: ({}, {})\t\tDirection: {}".format(self.state.pos_x, self.state.pos_y,
-                                                              direction_to_string(self.state.direction)))
+        self.log(f"Position: x: {self.state.pos_x}, y: {self.state.pos_y}, Direction: {self.state.direction}")
 
         # Debug
         #self.state.print_visit_count()
         #self.state.print_world_debug()
         self.state.print_heatmap()
 
-        return self.make_decision(bump, dirt, home, tiles, offsets)
+        return self.make_decision(bump, dirt, home, offsets)
 
-    def make_decision(self, bump, dirt, home, tiles, offsets):
+    def make_decision(self, bump, dirt, home, offsets):
 
         if bump:
             self.log(f"BUMP!\n\n")
 
         # Decide action
-        if self.check_if_finished(tiles):
+        if self.check_if_finished():
             self.log("ALL TILES VISITED!\n\n\n\n")
-            return self.return_home(tiles)
-        elif dirt:
+            if home:
+                self.log("ALL TILES VISITED AND AGENT HAS RETURNED HOME!\n\n")
+                self.log(f"Number of iterations: {MAX_ITERATIONS - self.iteration_counter}")
+                self.state.last_action = ACTION_NOP
+                return ACTION_NOP
+            else:
+                if not self.heatmap_reset:
+                    self.heatmap_reset = True
+                    self.reset_heatmap()
+                self.create_heatmap_slope()
+
+        if dirt:
             self.log("DIRT -> choosing SUCK action!")
             self.state.last_action = ACTION_SUCK
             return ACTION_SUCK
@@ -250,41 +262,29 @@ class MyVacuumAgent(Agent):
             next_direction = self.get_best_direction(offsets)
             if next_direction == 1:
                 self.log("Front tile is best -> going forward!")
+                self.turns_without_forward = 0
                 return self.go_forward()
-            elif next_direction == 0:
-                self.log("Left tile is best -> turning left!")
-                return self.turn_left()
             else:
-                self.log("Right tile is best -> turning right!")
-                return self.turn_right()
+                if self.turns_without_forward > self.max_turns_without_forward:
+                    self.log("Too many turns without going forward -> going forward!")
+                    self.turns_without_forward = 0
+                    return self.go_forward()
+                if next_direction == 0:
+                    self.log("Left tile is best -> turning left!")
+                    self.turns_without_forward += 1
+                    return self.turn_left()
+                else:
+                    self.log("Right tile is best -> turning right!")
+                    self.turns_without_forward += 1
+                    return self.turn_right()
 
-    def get_best_tile(self, tiles):
-        
-        best_tile = None
-        best_value = float('-inf')
-
-        for tile in tiles:
-            # Check if the tile is a wall or out of bounds
-            if tile["type"] == AGENT_STATE_WALL:
-                continue  # Skip walls
-            
-            # Check which tile has the highest value
-            if tile["heat"] > best_value:
-                best_tile = tile
-                best_value = tile["heat"]
-
-        #In case left, front and right tiles are all walls, turn randomly so that the agent doesn't get stuck
-        if best_tile is None:
-            self.log("No best tile found!")
-            pass
-            return self.turn_random()
-
-        return best_tile
 
     def get_best_direction(self, offsets):
         look_ahead_distance = 5
+        forward_bias_factor = 2
         direction_points = []
         for offset in offsets:
+            random_bonus_factor = random.uniform(0.9, 1.1)
             dx, dy = offset
             direction_score = 0
             tiles_checked = 0
@@ -297,10 +297,13 @@ class MyVacuumAgent(Agent):
                     if (self.state.world[x][y]["type"] == AGENT_STATE_WALL and distance == 1):
                         direction_score = float('-inf')
                         break
-            if tiles_checked == 0:
+            if tiles_checked < 1:
                 direction_score = float('-inf')
-            direction_points.append((direction_score / tiles_checked) + tiles_checked)
-            self.log(f"Direction: {offset} -> Score: {direction_score}")
+            if offset == offsets[1]:
+                self.log(f"Forward score: {direction_score}")
+                direction_score *= forward_bias_factor
+            direction_points.append(((direction_score / tiles_checked) + tiles_checked) * random_bonus_factor)
+        self.log(f"Direction points: {direction_points}")
         return direction_points.index(max(direction_points))
 
     def find_unchecked_tiles(self):
@@ -331,29 +334,27 @@ class MyVacuumAgent(Agent):
         else:
             return self.turn_right()
 
-    def check_if_finished(self, tiles):
+    def check_if_finished(self):
         for y in range(1, self.state.world_height - 1):
             for x in range(1, self.state.world_width - 1):
                 if self.state.world[x][y]["type"] == AGENT_STATE_UNKNOWN:
                     return False
         self.log("ALL TILES VISITED!\n\n\n\n")
-        return self.return_home(tiles)
-    
-    def return_home(self, tiles):
-        if self.state.pos_x == 1 and self.state.pos_y == 1:
-            self.log(f"Performance: {self.performance}")
-            self.log(f"Number of iterations: {1000 - self.iteration_counter}")
-            self.log("ALL TILES VISITED AND AGENT HAS RETURNED HOME!\n\n")
-            self.state.last_action = ACTION_NOP
-            return ACTION_NOP
-        else:
-            self.log(f"All tiles visited, returning home! Number of iterations: {self.iteration_counter}")
-            for y in range(1, self.state.world_height - 1):
-                for x in range(1, self.state.world_width - 1):
-                    self.state.world[x][y]["heat"] = (x + y)**3
-        #Prioritize going forward if possible, to avoid unnecessarily turning into wall on way home
-        #if tiles[1]["type"] != AGENT_STATE_WALL:
-            #return self.go_forward()
+        return True
+
+    def reset_heatmap(self):
+        for y in range(self.state.world_height):
+            for x in range(self.state.world_width):
+                self.state.world[x][y]["heat"] = 0
+                self.state.world[x][y]["visit_count"] = 0
+                self.state.world[x][y]["recently_visited"] = 0
+
+    def create_heatmap_slope(self):
+        for y in range(self.state.world_height):
+            for x in range(self.state.world_width):
+                if self.state.world[x][y]["type"] != AGENT_STATE_WALL:
+                    self.state.world[x][y]["visit_count"] += (x + y)**2
+        self.log("Slope created!")
 
     def update_recently_visited(self):
         for y in range(self.state.world_height):
@@ -364,36 +365,36 @@ class MyVacuumAgent(Agent):
                     self.state.world[x][y]["recently_visited"] += 1
 
     def update_heat(self):
-        wall_penalty = -2
-        unknown_bonus = 2
-        visit_penalty_factor = -5
-        recently_visited_bonus_factor = 2
+        wall_penalty_factor = -3
+        unknown_bonus_factor = 5
+        visit_count_penalty_factor = -2
+        recently_visited_bonus_factor = 1
         for y in range(self.state.world_height):
             for x in range(self.state.world_width):
                 tile = self.state.world[x][y]
-                
                 if tile["type"] == AGENT_STATE_WALL:
-                    tile["heat"] = wall_penalty
+                    tile["heat"] = (MAX_ITERATIONS - self.iteration_counter) * wall_penalty_factor
                 else:   
-                    random_bonus_factor = random.uniform(0.9, 1.1)
-                    self.log(f"Random bonus factor: {random_bonus_factor}")
-                    tile["heat"] = (tile["visit_count"] * visit_penalty_factor + tile["recently_visited"] * recently_visited_bonus_factor) * random_bonus_factor
+                    tile["heat"] = (tile["visit_count"] * visit_count_penalty_factor + tile["recently_visited"] * recently_visited_bonus_factor) 
                     if tile["type"] == AGENT_STATE_UNKNOWN:
-                        tile["heat"] = 10
+                        tile["heat"] += unknown_bonus_factor * (MAX_ITERATIONS - self.iteration_counter)
 
     def update_heatmap(self):
         visit_counts = np.zeros((self.state.world_width, self.state.world_height))
         for y in range(self.state.world_height):
             for x in range(self.state.world_width):
                 if self.state.world[x][y]["type"] == AGENT_STATE_WALL:
+                    pass
                     visit_counts[x, y] = -1 
                 else:
-                    visit_counts[x, y] = self.state.world[x][y]["heat"] 
+                    pass
+                visit_counts[x, y] = self.state.world[x][y]["heat"] 
 
         masked_visit_counts = np.ma.masked_where(visit_counts == -1, visit_counts)
 
         # Calculate dynamic vmax
-        current_max_visits = np.max(masked_visit_counts) 
+        current_max_heat = np.max(masked_visit_counts) 
+        current_min_heat = np.min(masked_visit_counts)
 
         if self.heatmap is None:  # Create heatmap on first update
             # Use 'Reds' colormap for white to dark red
@@ -401,16 +402,16 @@ class MyVacuumAgent(Agent):
                 masked_visit_counts.T, 
                 cmap="Reds", 
                 interpolation="nearest", 
-                vmin=0,  # White starts at 0 visits
-                vmax=max(1, current_max_visits) # Adjust vmax as needed
+                vmin=current_min_heat,  # White starts at 0 visits
+                vmax=max(1, current_max_heat) # Adjust vmax as needed
             )
             self.cbar = self.fig.colorbar(self.heatmap, label="Visit Count")
             
             # Set wall color to blue
-            self.heatmap.cmap.set_bad('blue') 
+            #self.heatmap.cmap.set_bad('blue') 
         else:
             self.heatmap.set_data(masked_visit_counts.T)
-            self.heatmap.set_clim(vmin=0, vmax=max(1, current_max_visits))  # Update colorbar range
+            self.heatmap.set_clim(vmin=current_min_heat, vmax=max(1, current_max_heat))  # Update colorbar range
             self.cbar.update_normal(self.heatmap)  # Update colorbar
 
         self.ax.set_title("Heatmap of Agent's Visits")
@@ -428,7 +429,5 @@ class MyVacuumAgent(Agent):
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
 
-    def visualize_heatmap(self):
-        #TODO: Move visualization of the heatmap
-        pass
+
     
